f5972d916fd653ec8345ca38776f73bea999c09e
Indunil1992 <indunil@adroitlogic.com>
2020-10-09 16:07:06 +0530

    g

--- indu-sep9-350.js
+++ indu-sep9-350.js
@@ -1,4 +1,23 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
 exports.handler = async (event) => {
     // 1st commnt
-    return {"message": "Successfully executed"};
+    try {
+        let data = await s3.listObjects({
+            Bucket: "111-vbvbw",
+            MaxKeys: 10
+        }).promise();
+
+    } catch (err) {
+        // error handling goes here
+    };
+    // 2nd commnt
+    //33
+    // 77
+    //00000
+    //oooo
+    //mmmmmmmmmmm
+    //
+    return { "message": "Successfully executed h" };
 };
